fba4c41b165e185d716055855713768ebece8dc2
alessandrofrancescostellato <unabirraodue@gmail.com>
2023-09-13 13:27:49 +0200

    Initial commit

---
+++ src/main/java/com/example/demores/Persona/Persona.java
@@ -0,0 +1,20 @@
+package com.example.demores.Persona;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Persona {
+
+    String id;
+    String nome;
+    String cognome;
+    String email;
+
+
+
+}
---
+++ src/test/java/com/example/demores/DemoResApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoResApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
